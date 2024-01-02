d84f2134c7506ce63925ee4d73cf782c0e1ffdae
jinho <144031458+NiceGuy1313@users.noreply.github.com>
2024-01-02 21:20:00 +0900

    Title: 세 실수의 반올림, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240102/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+	double a, b,c;
+    cout.precision(3);
+	cin >> a >> b >> c;
+	cout << a << endl << b << endl << c;
+
+	return 0;
+
+}
