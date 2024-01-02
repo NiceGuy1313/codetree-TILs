e51a918f94a5c735524a259ca0225022c845a2e9
jinho <144031458+NiceGuy1313@users.noreply.github.com>
2024-01-02 23:41:22 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240102/합과 평균/sum-and-avg.cpp
+++ 240102/합과 평균/sum-and-avg.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    cout << fixed;
     int a, b;
     cin >> a >> b;
+    cout.precision(1);
     cout << a+b << " " << (double)(a+b)/2;
 
     return 0;
