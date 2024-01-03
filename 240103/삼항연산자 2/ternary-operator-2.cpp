a88ad1ce271fab393909b42e2df25abd07a9fe2a
jinho <144031458+NiceGuy1313@users.noreply.github.com>
2024-01-03 22:28:21 +0900

    Title: 삼항연산자 2, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240103/삼항연산자 2/ternary-operator-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	int a;
+    string b;
+	cin >> a;
+
+    b = a == 1 ? "t" : "f";
+
+    cout << b;
+
+	return 0;
+
+}
